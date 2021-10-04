5424cc1996501b1cbc7cb2cfad7bac6df52aee60
Prashanth <imprash4u@gmail.com>
2021-10-05 00:57:21 +0530

    Added: Checking for duplicates in the AddressBook and eliminating

--- src/main/java/com/bridgelabz/addressbooksystem/AddressBookMainClass.java
+++ src/main/java/com/bridgelabz/addressbooksystem/AddressBookMainClass.java
@@ -255,16 +255,24 @@ public class AddressBookMainClass {
         boolean isKeyPresent = addressBookSystem.containsKey(addressBookName);
         if (isKeyPresent) {
             AddressBook values = addressBookSystem.get(addressBookName);
-            addressBookSystem.get(addressBookName);
             List<Contact> contactDetails = values.getContacts();
-            contactDetails.add(contact);
-            values.setContacts(contactDetails);
-            addressBookSystem.put(addressBookName, values);
+            boolean isPresent=   contactDetails.stream().anyMatch(con->con.getFirstName().equals(contact.getFirstName())) ;
+            if(isPresent) {
+
+                System.out.println("This peson name already persent");
+
+            }else {
+                contactDetails.add(contact);
+                values.setContacts(contactDetails);
+                addressBookSystem.put(addressBookName, values);
+            }
+
         } else {
             contactsLis.add(contact);
             Addressvalues.setContacts(contactsLis);
             addressBookSystem.put(addressBookName, Addressvalues);
         }
     }
+
 
 }
