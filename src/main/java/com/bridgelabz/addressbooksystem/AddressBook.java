1b8605811d4e597a09802fe566c67c62f778306c
Prashanth <imprash4u@gmail.com>
2021-10-05 00:40:33 +0530

    Added: Editing Existing contact in AddressBook

---
+++ src/main/java/com/bridgelabz/addressbooksystem/AddressBook.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.addressbooksystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    private List<Contact> contacts;
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
+
+}
