895f6cbd5d2509e9fb1d5f4a850ae96fa5dbab9c
Prashanth <imprash4u@gmail.com>
2021-10-05 00:19:43 +0530

    Added: Adding NewContact to AdddressBook

--- src/main/java/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -3,10 +3,11 @@ package com.bridgelabz.addressbooksystem;
 import java.util.Scanner;
 
 public class AddressBookMain {
+
     public void createContact() {
 
-        PersonContact contact = new PersonContact();
-        Scanner sc = new Scanner(System.in);
+        PersonContact contact =new PersonContact();
+        Scanner sc= new Scanner(System.in);
 
         System.out.print("\nEnter First Name  : ");
         String firstName = sc.nextLine();
@@ -42,20 +43,20 @@ public class AddressBookMain {
         showContact(contact);
     }
 
-    public void showContact(PersonContact contact) {
+    public void showContact(PersonContact contact){
 
         System.out.print("\n-----------------");
-        System.out.print("\nFirst Name  : " + contact.getFirstName());
-        System.out.print("\nLast Name   : " + contact.getLastName());
-        System.out.print("\nAddress     : " + contact.getAddress());
-        System.out.print("\nCity        : " + contact.getCity());
-        System.out.print("\nState       : " + contact.getState());
-        System.out.print("\nPhone Number : " + contact.getPhoneNo());
-        System.out.print("\nE-mail      : " + contact.getEmail());
-        System.out.print("\nZip         : " + contact.getZip());
+        System.out.print("\nFirst Name  : " +  contact.getFirstName());
+        System.out.print("\nLast Name   : " +  contact.getLastName());
+        System.out.print("\nAddress     : " +  contact.getAddress());
+        System.out.print("\nCity        : " +  contact.getCity());
+        System.out.print("\nState       : " +  contact.getState());
+        System.out.print("\nPhone Number : " +  contact.getPhoneNo());
+        System.out.print("\nE-mail      : " +  contact.getEmail());
+        System.out.print("\nZip         : " +  contact.getZip());
     }
 
-    public static void main(String[] args) {
+    public static void main( String[] args ){
 
         AddressBookMain book = new AddressBookMain();
         book.createContact();
